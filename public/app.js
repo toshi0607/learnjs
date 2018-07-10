ccc954494896ca106e8f9171249a5c5212e985c7
toshi0607 <s.toshi0607@gmail.com>
2018-07-10 09:57:58 +0900

    subcribes to the hash chaubscribes to the hash change event

--- public/app.js
+++ public/app.js
@@ -19,5 +19,8 @@ learnjs.showView = function(hash) {
 }
 
 learnjs.appOnReady = function() {
+  window.onhashchange = function() {
+    learnjs.showView(window.location.hash);
+  };
   learnjs.showView(window.location.hash);
 }
