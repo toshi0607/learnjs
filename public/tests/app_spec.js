bcfb4964e4ab8ff251a1d074d8d5e1a081a7d328
toshi0607 <s.toshi0607@gmail.com>
2018-07-11 10:17:54 +0900

    :sparkles: after last answer

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -28,6 +28,22 @@ describe('LearnJS', function() {
     expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
   });
 
+  it('can flash an element while setting the text', function() {
+    var elem = $('<p>');
+    spyOn(elem, 'fadeOut').and.callThrough();
+    spyOn(elem, 'fadeIn');
+    learnjs.flashElement(elem, "new text");
+    expect(elem.text()).toEqual("new text");
+    expect(elem.fadeOut).toHaveBeenCalled();
+    expect(elem.fadeIn).toHaveBeenCalled();
+  });
+
+  it('can redirect to the main view after the last problem is answered', function() {
+    var flash = learnjs.buildCorrectFlash(2);
+    expect(flash.find('a').attr('href')).toEqual("");
+    expect(flash.find('a').text()).toEqual("You're Finished!");
+  });
+
   describe('problem view', function() {
     var view;
     beforeEach(function() {
@@ -47,16 +63,36 @@ describe('LearnJS', function() {
     });
 
     describe('answer section', function() {
-      it('can check a correct answer by hitting a button', function() {
-        view.find('.answer').val('true');
-        view.find('.check-btn').click();
-        expect(view.find('.result').text()).toEqual('Correct!');
+      var resultFlash;
+
+      beforeEach(function() {
+        spyOn(learnjs, 'flashElement');
+        resultFlash = view.find('.result');
+      });
+
+      describe('when the answer is correct', function() {
+        beforeEach(function() {
+          view.find('.answer').val('true');
+          view.find('.check-btn').click();
+        });
+
+        it('flashes the result', function() {
+          var flashArgs = learnjs.flashElement.calls.argsFor(0);
+          expect(flashArgs[0]).toEqual(resultFlash);
+          expect(flashArgs[1].find('span').text()).toEqual('Correct!');
+        });
+
+        it('shows a link to the next problem', function() {
+          var link = learnjs.flashElement.calls.argsFor(0)[1].find('a');
+          expect(link.text()).toEqual('Next Problem');
+          expect(link.attr('href')).toEqual('#problem-2');
+        });
       });
 
       it('rejects an incorrect answer', function() {
         view.find('.answer').val('false');
         view.find('.check-btn').click();
-        expect(view.find('.result').text()).toEqual('Incorrect!');
+        expect(learnjs.flashElement).toHaveBeenCalledWith(resultFlash, 'Incorrect!');
       });
     });
   })
